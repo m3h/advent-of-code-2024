e0dcb03c1b100fdcdf1c10d4e65fb7d1ee7dd171
Reece Volker <reece@volker.page>
2024-12-03 06:16:05 +0100

    day 2

---
+++ day02b.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+from aocd import data, submit
+
+data1 = """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"""
+
+reports = []
+for line in data.splitlines():
+    reports.append([int(x) for x in line.split()])
+
+c = 0
+for report_big in reports:
+
+    for i in range(-1, len(report_big)):
+
+        report = list(report_big)
+        if i >= 0:
+            report.pop(i)
+
+
+        not_decreasing = False
+        not_increasing = False
+        very_unsafe = False
+        for i in range(1, len(report)):
+            
+
+            if report[i] >= report[i-1]:
+                not_decreasing = True
+            if report[i] <= report[i-1]:
+                not_increasing = True
+            d = abs(report[i] - report[i-1]) 
+            if d < 1 or d > 3:
+                very_unsafe = True
+
+            if not_decreasing and not_increasing:
+                very_unsafe = True
+
+        if not very_unsafe:
+            print(report)
+            c += 1        
+            break
+
+
+print(c)
+
+# print(ans)
+# submit(ans)
